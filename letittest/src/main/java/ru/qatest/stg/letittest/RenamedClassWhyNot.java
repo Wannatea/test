7dd70608ac6d3e25349f2cc7c41e9a49859fbf7d
Wannatea <glebka94@gmail.com>
2021-02-07 15:10:17 +0300

    Добавил методы с площадями

--- letittest/src/main/java/ru/qatest/stg/letittest/RenamedClassWhyNot.java
+++ letittest/src/main/java/ru/qatest/stg/letittest/RenamedClassWhyNot.java
@@ -1,7 +1,32 @@
 package ru.qatest.stg.letittest;
 
-public class RenamedClassWhyNot {
+public class RenamedClassWhyNot<h> {
     public static void main(String[] args) {
-        System.out.println("MAZAFAKA");
+        testFunc("FAKA");
+        testFunc("FACA");
+        testFunc("FAKKA");
+        testFunc("FAKHA");
+        testFunc("FAHKA");
+
+        double length = 9;
+        System.out.println("Площ квадр со стор " + length + "=" + plosh(length));
+        
+        double len = 4;
+        double h=10;
+        System.out.println("Площ прямоуг со сторонами " + length + " и " + h + plosh(len,h));
+    }
+
+    public static void testFunc(String text1) {
+
+        System.out.println("MAZA " + text1 + " ?");
+    }
+
+    public static double plosh(double l) {
+        return l * l;
+    }
+
+
+    public static double plosh(double len, double h) {
+        return len * h;
     }
 }
